f0d9a4da08f974bb5a8021e3ee342d4dcbad3090
Marwan Alani <marwan@alani.ca>
2017-03-08 15:30:26 -0500

    Implementing all Delegate functions in Create View

--- Meme Generator/CreateViewController.swift
+++ Meme Generator/CreateViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
-class CreateViewController: UIViewController {
+// MARK: - CreateViewController base declarations & interfaces
+class CreateViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var saveButton: UIBarButtonItem!
     @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var actionButton: UIBarButtonItem!
     @IBOutlet weak var addButton: UIBarButtonItem!
     
+    let memeTextAttributes:[String:Any] = [
+        NSStrokeColorAttributeName: UIColor.black,
+        NSForegroundColorAttributeName: UIColor.white,
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName: -3.0]
+    
+    var editingTopText = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+        self.imageButtonEnabler()
+        topText.delegate = self
+        topText.defaultTextAttributes = memeTextAttributes
+        topText.textAlignment = .center
+        bottomText.delegate = self
+        bottomText.defaultTextAttributes = memeTextAttributes
+        bottomText.textAlignment = .center
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.subscribeToKeyboardNotifications()
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,24 +52,103 @@ class CreateViewController: UIViewController {
     @IBAction func actionButtonPressed(_ sender: Any) {
     }
     
-    @IBAction func cameraButtonPressed(_ sender: Any) {
-    }
-    
     @IBAction func saveButtonPressed(_ sender: Any) {
     }
     
     @IBAction func cancelButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+// MARK: - UIImagePickerControllerDelegate Functions
+extension CreateViewController {
+    @IBAction func addButtonPressed(_ sender: Any) {
+        let pickController = UIImagePickerController()
+        pickController.delegate = self
+        pickController.sourceType = .photoLibrary
+        self.present(pickController, animated: true, completion: nil)
     }
-    */
+    
+    @IBAction func cameraButtonPressed(_ sender: Any) {
+        let pickController = UIImagePickerController()
+        pickController.delegate = self
+        pickController.sourceType = .camera
+        self.present(pickController, animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.imageButtonEnabler()
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+             self.imageView.image = image
+        }
+        self.imageButtonEnabler()
+        dismiss(animated: true, completion: nil)
+    }
+}
 
+// MARK: - UITextFieldDelegate Functions
+extension CreateViewController {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.tag == 1 {
+            textField.text = textField.text == "TOP" ? "" : textField.text
+            self.editingTopText = true
+        } else {
+            textField.text = textField.text == "BOTTOM" ? "" : textField.text
+            self.editingTopText = false
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+
+// MARK: - Notification & Keyboard Helper Functions
+extension CreateViewController {
+    func keyboardWillShow(_ notification:Notification) {
+        if !self.editingTopText {
+            view.frame.origin.y = 0 - getKeyboardHeight(notification)
+        }
+    }
+    
+    func keyboardWillHide(_ notification: Notification) {
+        if !self.editingTopText {
+            view.frame.origin.y = 0
+            self.editingTopText = false
+        }
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(_:)), name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(_:)), name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.cgRectValue.height
+    }
+}
+
+// MARK: - Other Helper Functions
+extension CreateViewController {
+    func imageButtonEnabler() {
+        if self.imageView.image == nil {
+            self.saveButton.isEnabled = false
+            self.actionButton.isEnabled = false
+        } else {
+            self.saveButton.isEnabled = true
+            self.actionButton.isEnabled = true
+        }
+    }
 }
