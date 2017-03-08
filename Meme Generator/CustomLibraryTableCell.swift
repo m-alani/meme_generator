eb9e1594abeb6e776a0a1a113836eee444a4339a
Marwan Alani <marwan@alani.ca>
2017-03-07 20:26:58 -0500

    Set the default Table Cell

--- Meme Generator/CustomLibraryTableCell.swift
+++ Meme Generator/CustomLibraryTableCell.swift
@@ -10,15 +10,16 @@ import UIKit
 
 class CustomLibraryTableCell: UITableViewCell {
 
+    @IBOutlet weak var cellImage: UIImageView!
+    @IBOutlet weak var cellTopLabel: UILabel!
+    @IBOutlet weak var cellBottomLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
 }
--- Meme Generator/LibraryTableViewController.swift
+++ Meme Generator/LibraryTableViewController.swift
@@ -36,20 +36,14 @@ extension LibraryTableViewController : UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell")!
-        cell.imageView?.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        cell.imageView?.backgroundColor = UIColor(colorLiteralRed: 0.15, green: 0.94, blue: 0.36, alpha: 1)
-        cell.imageView?.contentMode = .scaleAspectFit
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "Cell") as! CustomLibraryTableCell
 
         if memesList.count == 0 {
-            cell.textLabel?.text = "It's emptier than a black hole here!"
-            cell.detailTextLabel?.text = "GO MAKE SOME MEMES!"
-            cell.imageView?.image = #imageLiteral(resourceName: "Black_hole")
             cell.isUserInteractionEnabled = false
         } else {
-            cell.textLabel?.text = memesList[indexPath.row].topText
-            cell.detailTextLabel?.text = memesList[indexPath.row].bottomText
-            cell.imageView?.image = memesList[indexPath.row].originalImg
+            cell.cellTopLabel.text = memesList[indexPath.row].topText
+            cell.cellBottomLabel.text = memesList[indexPath.row].bottomText
+            cell.cellImage.image = memesList[indexPath.row].originalImg
         }
         
         return cell
